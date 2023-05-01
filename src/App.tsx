c21428181adfb01ec2bb69a5bc425ed567af8dc5
richslezak <richslezak@gmail.com>
2023-05-01 16:45:18 -0700

    added context provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,15 @@
-import { useState } from 'react';
 import NewTodo from './components/NewTodo';
 import Todos from './components/Todos';
-import Todo from './models/todo';
 import './App.css';
+import TodosContextProvider from './store/todos-context';
 
 function App() {
-  const [todos, setTodos] = useState<Todo[]>([]);
-
-  const addTodoHandler = (todoText: string) => {
-    const newTodo = new Todo(todoText);
-    setTodos((prevTodos) => {
-      return prevTodos.concat(newTodo);
-    });
-  };
-
-  const removeTodoHandler = (todoId: string) => {
-    setTodos((prevTodos) => {
-      return prevTodos.filter((todo) => todo.id !== todoId);
-    });
-  };
-
   return (
-    <>
+    <TodosContextProvider>
       <h1>Todo List</h1>
-      <NewTodo onAddTodo={addTodoHandler} />
-      <Todos items={todos} onRemoveTodo={removeTodoHandler} />
-    </>
+      <NewTodo />
+      <Todos />
+    </TodosContextProvider>
   );
 }
 
---
+++ src/store/todos-context.tsx
@@ -0,0 +1,50 @@
+import { createContext, useState } from 'react';
+import Todo from '../models/todo';
+
+type TodosContextObj = {
+  items: Todo[];
+  addTodo: (text: string) => void;
+  removeTodo: (id: string) => void;
+};
+
+type Props = {
+  children?: React.ReactNode;
+};
+
+export const TodosContext = createContext<TodosContextObj>({
+  items: [],
+  addTodo: () => {
+    // adds todo to list
+    // note to self: need to add comment otherwise eslint will throw an error on emply method
+  },
+  removeTodo: () => {
+    // removes todo from list
+  },
+});
+
+const TodosContextProvider: React.FC<Props> = ({ children }) => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText);
+    setTodos((prevTodos) => {
+      return prevTodos.concat(newTodo);
+    });
+  };
+
+  const removeTodoHandler = (todoId: string) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.id !== todoId);
+    });
+  };
+
+  const contextValue = {
+    items: todos,
+    addTodo: addTodoHandler,
+    removeTodo: removeTodoHandler,
+  };
+
+  return <TodosContext.Provider value={contextValue}>{children}</TodosContext.Provider>;
+};
+
+export default TodosContextProvider;
